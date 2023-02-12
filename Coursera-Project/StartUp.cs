b3d3cc880ed2065b50d2ba8747d1c1755f3023a4
Ivan Iliev <ivanilievdackel@gmail.com>
2023-02-12 14:16:18 +0200

    SerializeObjects to CSV file

--- Coursera-Project/StartUp.cs
+++ Coursera-Project/StartUp.cs
@@ -1,6 +1,17 @@
 ﻿using Coursera_Project.Data;
+using CsvHelper;
+using System.Globalization;
 
 CourseraDbContext context = new CourseraDbContext();
 //context.Database.EnsureDeleted();
 context.Database.EnsureCreated();
+
+
+var records = context.Students.ToList();
+
+using (var writer = new StreamWriter("E:\\GitHub\\Coursera\\Coursera\\OutputFiles\\file.csv"))
+using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+{
+    csv.WriteRecords(records);
+}
 
